579889bdd31ff880abc3db80086e8e9fd7dbdb7a
Kevin Kobori <kevinkobori@MacBook-Pro-de-Kevin.local>
2022-01-15 08:37:53 -0400

    fix TextFormField CEP with MaskedTextField package

--- lib/screens/address/components/cep_input_field.dart
+++ lib/screens/address/components/cep_input_field.dart
@@ -5,9 +5,9 @@ import 'package:lojavirtual/common/custom_icon_button.dart';
 import 'package:lojavirtual/models/address.dart';
 import 'package:lojavirtual/models/cart_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:masked_text/masked_text.dart';
 
 class CepInputField extends StatefulWidget {
-
   const CepInputField(this.address);
 
   final Address address;
@@ -17,7 +17,6 @@ class CepInputField extends StatefulWidget {
 }
 
 class _CepInputFieldState extends State<CepInputField> {
-
   final TextEditingController cepController = TextEditingController();
 
   @override
@@ -25,51 +24,64 @@ class _CepInputFieldState extends State<CepInputField> {
     final cartManager = context.watch<CartManager>();
     final primaryColor = Theme.of(context).primaryColor;
 
-    if(widget.address.zipCode == null)
+    if (widget.address.zipCode == null)
       return Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          TextFormField(
-            enabled: !cartManager.loading,
-            controller: cepController,
-            decoration: const InputDecoration(
+          MaskedTextField(
+            maskedTextFieldController: cepController,
+            escapeCharacter: '#',
+            mask: "##.###-###",
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            inputDecoration: const InputDecoration(
               isDense: true,
               labelText: 'CEP',
-              hintText: '12.345-678'
+              hintText: '12.345-678',
             ),
-            inputFormatters: [
-              // WhitelistingTextInputFormatter.digitsOnly,
-              CepInputFormatter(),
-            ],
-            keyboardType: TextInputType.number,
-            validator: (cep){
-              if(cep.isEmpty)
-                return 'Campo obrigatório';
-              else if(cep.length != 10)
-                return 'CEP Inválido';
-              return null;
-            },
-          ),
-          if(cartManager.loading)
+          ), // TODO VALIDATE
+          // TextFormField(
+          //   enabled: !cartManager.loading,
+          //   controller: cepController,
+          //   decoration: const InputDecoration(
+          //     isDense: true,
+          //     labelText: 'CEP',
+          //     hintText: '12.345-678',
+          //   ),
+          //   inputFormatters: [
+          //     // WhitelistingTextInputFormatter.digitsOnly,
+          //     CepInputFormatter(),
+          //   ],
+          //   keyboardType: TextInputType.number,
+          //   validator: (cep) {
+          //     if (cep.isEmpty)
+          //       return 'Campo obrigatório';
+          //     else if (cep.length != 10) return 'CEP Inválido';
+          //     return null;
+          //   },
+          // ),
+          if (cartManager.loading)
             LinearProgressIndicator(
               valueColor: AlwaysStoppedAnimation(primaryColor),
               backgroundColor: Colors.transparent,
             ),
           RaisedButton(
-            onPressed: !cartManager.loading ? () async {
-              if(Form.of(context).validate()){
-                try {
-                  await context.read<CartManager>().getAddress(cepController.text);
-                } catch (e){
-                  Scaffold.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('$e'),
-                      backgroundColor: Colors.red,
-                    )
-                  );
-                }
-              }
-            } : null,
+            onPressed: !cartManager.loading
+                ? () async {
+                    if (Form.of(context).validate()) {
+                      try {
+                        await context
+                            .read<CartManager>()
+                            .getAddress(cepController.text);
+                      } catch (e) {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                          content: Text('$e'),
+                          backgroundColor: Colors.red,
+                        ));
+                      }
+                    }
+                  }
+                : null,
             textColor: Colors.white,
             color: primaryColor,
             disabledColor: primaryColor.withAlpha(100),
@@ -85,17 +97,15 @@ class _CepInputFieldState extends State<CepInputField> {
             Expanded(
               child: Text(
                 'CEP: ${widget.address.zipCode}',
-                style: TextStyle(
-                  color: primaryColor,
-                  fontWeight: FontWeight.w600
-                ),
+                style:
+                    TextStyle(color: primaryColor, fontWeight: FontWeight.w600),
               ),
             ),
             CustomIconButton(
               iconData: Icons.edit,
               color: primaryColor,
               size: 20,
-              onTap: (){
+              onTap: () {
                 context.read<CartManager>().removeAddress();
               },
             ),
